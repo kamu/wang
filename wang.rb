15176b44c32131a97d61bb80df3da7e26a754afd
joujoujou <joujoujou@6e88d453-e347-0410-bde2-a3cdfb4bac30>
2008-03-14 13:16:54 +0000

    namespace WANG: WANG => WANG::Client, WANGJar => WANG::Jar, WANGCookie => WANG::Cookie timeouts fixed a possible bug in chunked body reading
    git-svn-id: http://unaustralian.org.au/ruby@19 6e88d453-e347-0410-bde2-a3cdfb4bac30

--- wang.rb
+++ wang.rb
@@ -17,11 +17,14 @@ require 'stringio'
 require 'zlib'
 require 'logger'
 require 'yaml'
+require 'timeout'
 
-REDIRECTION_CODES = [301, 302, 303, 307]
+module WANG
 
-# all the predefined headers should end with \n, so they can easily be added together
-HEADERS = 
+	REDIRECTION_CODES = [301, 302, 303, 307]
+
+	# all the predefined headers should end with \n, so they can easily be added together
+	HEADERS = 
 "%s %s HTTP/1.1
 Host: %s
 User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12
@@ -32,282 +35,296 @@ Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
 Keep-Alive: 300
 Connection: keep-alive
 Referer: %s\n"
-COOKIES = "Cookie: %s\n"
-FORM = 
+	COOKIES = "Cookie: %s\n"
+	FORM = 
 "Content-Type: application/x-www-form-urlencoded
 Content-Length: %s\n"
+	DEFAULT_OPEN_TIMEOUT = 60
+	DEFAULT_READ_TIMEOUT = 60
 
-
-class WANG
-	attr_accessor :referer
-
-	def initialize
-		@log = Logger.new(STDOUT)
-		@log.level = Logger::DEBUG
-
-		@jar = WANGJar.new
-		@socket = nil
-		@host = nil
-		@referer = URI.parse("http://www.google.com/")
+	def self.new(*args)
+		Client.new(*args)
 	end
 
-	def get url, referer = nil
-		@log.debug("GETTING: #{url.to_s}")
-		request("GET", url.is_a?(URI) ? url : URI.parse(url), referer) 
-	end
+	class Client
+		attr_accessor :referer
 
-	def post url, data, referer = nil
-		@log.debug("POSTING: #{url.to_s}")
-		request("POST", url.is_a?(URI) ? url : URI.parse(url), referer, data) 
-	end
+		def initialize args = {}
+			@log = Logger.new(STDOUT)
+			@log.level = Logger::DEBUG
 
-	def save_cookies file
-		@jar.save(file)
-	end
+			@jar = Jar.new
+			@socket = nil
+			@host = nil
+			@referer = URI.parse("http://www.google.com/")
+			@read_timeout = args[:read_timeout] || DEFAULT_READ_TIMEOUT
+			@open_timeout = args[:open_timeout] || DEFAULT_OPEN_TIMEOUT
 
-	def load_cookies file
-		@jar.load(file)
-	end
-
-	private
-	def request method, uri, referer = nil, data = nil
-		check_socket uri.host
-
-		@referer = referer.nil? ? @referer : referer
-
-		@socket << HEADERS % [
-			method,
-			uri.path.empty? ? "/" : uri.path + (uri.query.nil? ? "" : "?#{uri.query}"),
-			uri.host, @referer.to_s
-		]
-		
-		@socket << COOKIES % @jar.cookies_for(uri) unless @jar.cookies_for(uri).empty?
-		@log.debug("SENDING COOKIES: #{@jar.cookies_for(uri)}")
-
-		data = data.map {|k,v| "#{URI.encode(k)}=#{URI.encode(v)}"}.join("&") if data.is_a?(Hash)
-
-		@socket << FORM % data.length if data
-		@socket << "\n"
-		@socket << data if data
-
-		@referer = uri
-
-		status = read_status
-		@log.debug("STATUS: #{status}")
-		headers = read_headers
-		@log.debug("HEADERS: #{headers.inspect}")
-		body = read_body(headers)
-		@log.debug("WANGJAR: #{@jar.inspect}")
-
-		@socket.close if headers["connection"] =~ /close/
-
-		return handle_redirect(headers["location"], uri) if REDIRECTION_CODES.include?(status)
-		body = decompress(headers["content-encoding"], body)
-
-		return status, headers, body
-	end
-
-	def read_status
-		line = @socket.gets("\n")
-		status = line.match(%r{^HTTP/1\.\d (\d+) })[1]
-		return status.to_i
-	end
-
-	def read_headers
-		headers = Hash.new
-		while header = @socket.gets("\n")
-			header.chomp!
-			break if header.empty?
-
-			key, val = header.split(": ", 2)
-			if key =~ /^Set-Cookie2?$/i #do we dare consider set-cookie2 the same?
-				@jar.consume(val, @referer)
-			else
-				headers.store(key.downcase, val)
-			end
+			@log.debug("Using #{@read_timeout} as the read timeout and #{@open_timeout} as the open timeout")
 		end
 
-		return headers
-	end
+		def get url, referer = nil
+			@log.debug("GETTING: #{url.to_s}")
+			request("GET", url.is_a?(URI) ? url : URI.parse(url), referer) 
+		end
 
-	def read_body headers
-		body = ""
-		if headers["transfer-encoding"] =~ /chunked/i # read chunked body
-			while true
-				line = @socket.readline
-				chunk_len = line.slice(/[0-9a-fA-F]+/).hex
-				break if chunk_len == 0
-				body << @socket.read(chunk_len)
-				@socket.read 2 # read the damn linechange
-			end
-			until (line = @socket.gets) and (line.nil? or line.sub(/\r?\n?/, "").empty?); end # read the chunk footers and the last line
-		elsif headers["content-length"]
-			clen = headers["content-length"].to_i
-			while body.length < clen
-				body << @socket.read([clen - body.length, 4096].min)
-			end
-		else #fallback that'll just consume all the data available 
-			begin
-				while true
-					body << @socket.readpartial(4096)
+		def post url, data, referer = nil
+			@log.debug("POSTING: #{url.to_s}")
+			request("POST", url.is_a?(URI) ? url : URI.parse(url), referer, data) 
+		end
+
+		def save_cookies file
+			@jar.save(file)
+		end
+
+		def load_cookies file
+			@jar.load(file)
+		end
+
+		private
+		def request method, uri, referer = nil, data = nil
+			check_socket uri.host
+
+			@referer = referer.nil? ? @referer : referer
+
+			@socket << HEADERS % [
+				method,
+				uri.path.empty? ? "/" : uri.path + (uri.query.nil? ? "" : "?#{uri.query}"),
+				uri.host, @referer.to_s
+			]
+
+			@socket << COOKIES % @jar.cookies_for(uri) unless @jar.cookies_for(uri).empty?
+			@log.debug("SENDING COOKIES: #{@jar.cookies_for(uri)}")
+
+			data = data.map {|k,v| "#{URI.encode(k)}=#{URI.encode(v)}"}.join("&") if data.is_a?(Hash)
+
+			@socket << FORM % data.length if data
+			@socket << "\n"
+			@socket << data if data
+
+			@referer = uri
+
+			status = read_status
+			@log.debug("STATUS: #{status}")
+			headers = read_headers
+			@log.debug("HEADERS: #{headers.inspect}")
+			body = read_body(headers)
+			@log.debug("WANGJAR: #{@jar.inspect}")
+
+			@socket.close if headers["connection"] =~ /close/
+
+				return handle_redirect(headers["location"], uri) if REDIRECTION_CODES.include?(status)
+			body = decompress(headers["content-encoding"], body)
+
+			return status, headers, body
+		end
+
+		def read_status
+			line = Timeout::timeout(@open_timeout) { @socket.gets("\n") }
+			status = line.match(%r{^HTTP/1\.\d (\d+) })[1]
+			return status.to_i
+		end
+
+		def read_headers
+			headers = Hash.new
+			while header = Timeout::timeout(@read_timeout) { @socket.gets("\n") }
+				header.chomp!
+				break if header.empty?
+
+				key, val = header.split(": ", 2)
+				if key =~ /^Set-Cookie2?$/i #do we dare consider set-cookie2 the same?
+					@jar.consume(val, @referer)
+				else
+					headers.store(key.downcase, val)
 				end
-			rescue EOFError
 			end
+
+			return headers
 		end
 
-		return body
-	end
-
-	def handle_redirect location, olduri
-		@log.debug(location.inspect)
-		dest = URI.parse(location)
-		dest = olduri + dest unless dest.is_a?(URI::HTTP) # handle relative redirect
-		get(dest)
-	end
-
-	def decompress type, body
-		case type
-		when "gzip"
-			return Zlib::GzipReader.new(StringIO.new(body)).read
-		when "deflate"
-			begin
-				return Zlib::Inflate.inflate(body)
-			rescue Zlib::DataError # check http://www.ruby-forum.com/topic/136825 for more info
-				return Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(body)
+		def read_body headers
+			body = ""
+			if headers["transfer-encoding"] =~ /chunked/i # read chunked body
+				while true
+					line =  Timeout::timeout(@read_timeout) { @socket.readline }
+					chunk_len = line.slice(/[0-9a-fA-F]+/).hex
+					break if chunk_len == 0
+					while chunk_len > 0 # make sure to read the whole chunk
+						buf = Timeout::timeout(@read_timeout) { @socket.read(chunk_len) }
+						chunk_len -= buf.length
+						body << buf
+					end
+					Timeout::timeout(@read_timeout) { @socket.read 2 } # read the damn linechange
+				end
+				until (line = Timeout::timeout(@read_timeout) { @socket.gets }) and (line.nil? or line.sub(/\r?\n?/, "").empty?); end # read the chunk footers and the last line
+			elsif headers["content-length"]
+				clen = headers["content-length"].to_i
+				while body.length < clen
+					Timeout::timeout(@read_timeout) { body << @socket.read([clen - body.length, 4096].min) }
+				end
+			else #fallback that'll just consume all the data available 
+				begin
+					while true
+						Timeout::timeout(@read_timeout) { body << @socket.readpartial(4096) }
+					end
+				rescue EOFError
+				end
 			end
-		else
+
 			return body
 		end
-	end
 
-	def check_socket host 		
-		connect(host) if @socket.nil? or @socket.closed? or @host.nil? or not @host.eql? host
-	end
+		def handle_redirect location, olduri
+			@log.debug(location.inspect)
+			dest = URI.parse(location)
+			dest = olduri + dest unless dest.is_a?(URI::HTTP) # handle relative redirect
+			get(dest)
+		end
 
-	def connect host
-		@log.debug("Connecting to #{host}")
-		@socket.close unless @socket.nil? or @socket.closed?
-		@socket = TCPSocket.new(host, 'http')
-		@host = host
-	end
-end
-
-class WANGCookie
-	attr_accessor :key, :value, :domain, :path, :expires
-
-	def initialize key = nil, value = nil
-		@key, @value = key, value
-		@domain, @path, @expires = nil
-	end
-
-	def parse raw_cookie, uri = nil
-		keyval, *attributes = raw_cookie.split(/;\s*/)
-		@key, @value = keyval.split("=", 2)
-
-		attributes.each do |at|
-			case at
-			when /domain=(.*)/i
-				@domain = $1
-			when /expires=(.*)/i
-				@expires = begin
-					Time.parse($1)
-				rescue
-					nil
+		def decompress type, body
+			case type
+			when "gzip"
+				return Zlib::GzipReader.new(StringIO.new(body)).read
+			when "deflate"
+				begin
+					return Zlib::Inflate.inflate(body)
+				rescue Zlib::DataError # check http://www.ruby-forum.com/topic/136825 for more info
+					return Zlib::Inflate.new(-Zlib::MAX_WBITS).inflate(body)
 				end
-			when /path=(.*)/i
-				@path = $1
+			else
+				return body
 			end
 		end
 
-		@domain = uri.host if @domain.nil? and uri
-		@path = uri.path if @path.nil? and uri
+		def check_socket host 		
+			connect(host) if @socket.nil? or @socket.closed? or @host.nil? or not @host.eql? host
+		end
 
-		self
-	end
-
-	def same? c
-		self.key.eql? c.key and self.domain.eql? c.domain and self.path.eql? c.path
-	end
-
-	def match? uri
-		match_domain?(uri.host) and match_path?(uri.path)
-	end
-
-	def expired?
-		@expires.is_a?(Time) ? @expires < Time.now : false
-	end
-
-	def match_domain? domain # TODO check if this fully follows the spec
-		case @domain
-		when /^\d+\.\d+\.\d+\.\d+$/ # ip address X.X.X.X not .*X.X.X.X.* :)
-			domain.eql?(@domain)
-		when /^\./ # so domain = site.com and subdomains could match @domain = .site.com
-			#strings used in a regexp should be escaped, because it thinks . is regexp
-			#also, domains should be case insensitive matches
-			domain =~ /#{Regexp.escape(@domain)}$/i
-		else
-			domain.downcase.eql?(@domain.downcase)
+		def connect host
+			@log.debug("Connecting to #{host}")
+			@socket.close unless @socket.nil? or @socket.closed?
+			Timeout::timeout(@open_timeout) { @socket = TCPSocket.new(host, 'http') }
+			@host = host
 		end
 	end
 
-	def match_path? path
-		path =~ /^#{Regexp.escape(@path)}/
-		#FIXME: should a cookie path /cat match /catshouldie ? (surely not) (I need to read up the RFC on cookie paths.)
-	end
-end
+	class Cookie
+		attr_accessor :key, :value, :domain, :path, :expires
 
-class WANGJar
-	def initialize
-		@jar = []
-	end
+		def initialize key = nil, value = nil
+			@key, @value = key, value
+			@domain, @path, @expires = nil
+		end
 
-	def consume raw_cookie, uri = nil
-		cookie = WANGCookie.new.parse(raw_cookie, uri)
-		add(cookie)
-	end
+		def parse raw_cookie, uri = nil
+			keyval, *attributes = raw_cookie.split(/;\s*/)
+			@key, @value = keyval.split("=", 2)
 
-	def add c
-		i = index(c)
-		if i.nil?
-			@jar << c
-		else
-			@jar[i] = c
+			attributes.each do |at|
+				case at
+				when /domain=(.*)/i
+					@domain = $1
+				when /expires=(.*)/i
+					@expires = begin
+								   Time.parse($1)
+							   rescue
+								   nil
+							   end
+				when /path=(.*)/i
+					@path = $1
+				end
+			end
+
+			@domain = uri.host if @domain.nil? and uri
+			@path = uri.path if @path.nil? and uri
+
+			self
+		end
+
+		def same? c
+			self.key.eql? c.key and self.domain.eql? c.domain and self.path.eql? c.path
+		end
+
+		def match? uri
+			match_domain?(uri.host) and match_path?(uri.path)
+		end
+
+		def expired?
+			@expires.is_a?(Time) ? @expires < Time.now : false
+		end
+
+		def match_domain? domain # TODO check if this fully follows the spec
+			case @domain
+			when /^\d+\.\d+\.\d+\.\d+$/ # ip address X.X.X.X not .*X.X.X.X.* :)
+				domain.eql?(@domain)
+			when /^\./ # so domain = site.com and subdomains could match @domain = .site.com
+				#strings used in a regexp should be escaped, because it thinks . is regexp
+				#also, domains should be case insensitive matches
+				domain =~ /#{Regexp.escape(@domain)}$/i
+			else
+				domain.downcase.eql?(@domain.downcase)
+			end
+		end
+
+		def match_path? path
+			path =~ /^#{Regexp.escape(@path)}/
+				#FIXME: should a cookie path /cat match /catshouldie ? (surely not) (I need to read up the RFC on cookie paths.)
 		end
 	end
 
-	def cookies_for uri
-		@jar.select { |c| c.match?(uri) }.map{ |c| "#{c.key}=#{c.value}" }.join("; ")
-		# ^^ you have got to love ruby, if only purely for the above line.
-	end
-
-	def index c
-		@jar.each do |cookie|
-			return @jar.index(cookie) if cookie.same? c
+	class Jar
+		def initialize
+			@jar = []
 		end
 
-		nil
-	end
+		def consume raw_cookie, uri = nil
+			cookie = Cookie.new.parse(raw_cookie, uri)
+			add(cookie)
+		end
 
-	def include? c
-		not index(c).nil?
-	end
+		def add c
+			i = index(c)
+			if i.nil?
+				@jar << c
+			else
+				@jar[i] = c
+			end
+		end
 
-	def save file
-		file << @jar.to_yaml
-		file.close
-	end
+		def cookies_for uri
+			@jar.select { |c| c.match?(uri) }.map{ |c| "#{c.key}=#{c.value}" }.join("; ")
+			# ^^ you have got to love ruby, if only purely for the above line.
+		end
 
-	def load file
-		saved_jar = YAML::load(file.read)
+		def index c
+			@jar.each do |cookie|
+				return @jar.index(cookie) if cookie.same? c
+			end
 
-		saved_jar.each do |c|
-			add(c)
+			nil
+		end
+
+		def include? c
+			not index(c).nil?
+		end
+
+		def save file
+			file << @jar.to_yaml
+			file.close
+		end
+
+		def load file
+			saved_jar = YAML::load(file.read)
+
+			saved_jar.each do |c|
+				add(c)
+			end
 		end
 	end
 end
 
 if __FILE__ == $0
-	test = WANG.new
+	test = WANG.new({:open_timeout=>5})
 	#st, hd, bd = test.get("http://www.whatismyip.com")
 	#st, hd, bd = test.get("http://google.com")
 	#st, hd, bd = test.get("http://bash.org/?random1")
@@ -315,12 +332,12 @@ if __FILE__ == $0
 	#st, hd, bd = test.post('http://emmanuel.faivre.free.fr/phpinfo.php', 'mopar=dongs&joux3=king')
 	#st, hd, bd = test.post('http://emmanuel.faivre.free.fr/phpinfo.php', {'mopar'=>'dongs', 'joux3'=>'king'})
 	#st, hd, bd = test.get("http://www.myspace.com/")
-	
+
 	#this shit is getting seriously pro:
 	test.load_cookies(File.new("cookietest.txt", "r")) if File.exists?("cookietest.txt")
 	st, hd, bd = test.get("http://www.myspace.com/")
 	test.save_cookies(File.new("cookietest.txt", "w"))
-	
+
 	#puts [st, hd].inspect
 	#puts bd
 end
