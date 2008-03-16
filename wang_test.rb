c6c6f99e5bad1389d2a58525bd9c9ac8b2e82029
pd <pd@6e88d453-e347-0410-bde2-a3cdfb4bac30>
2008-03-16 17:35:55 +0000

    fix indents (tabs)
    git-svn-id: http://unaustralian.org.au/ruby@21 6e88d453-e347-0410-bde2-a3cdfb4bac30

--- wang_test.rb
+++ wang_test.rb
@@ -4,14 +4,14 @@ require 'test/unit'
 require 'wang'
 
 class WangTest < Test::Unit::TestCase
-  def setup
-    @client = WANG.new
-  end
+	def setup
+		@client = WANG.new
+	end
 
-  def test_returns_success_from_google
-    status, headers, body = @client.get('http://www.google.com')
-    assert_equal 200, status
-  end
+	def test_returns_success_from_google
+		status, headers, body = @client.get('http://www.google.com')
+		assert_equal 200, status
+	end
 
 	def test_returns_headers_hash
 		status, headers, body = @client.get('http://www.google.com')
