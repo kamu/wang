08d7856e966c4675cb7e2d2e192c233f2d8eb351
pd <pd@6e88d453-e347-0410-bde2-a3cdfb4bac30>
2008-03-16 17:29:15 +0000

    simple unit test
    git-svn-id: http://unaustralian.org.au/ruby@20 6e88d453-e347-0410-bde2-a3cdfb4bac30

---
+++ wang_test.rb
@@ -0,0 +1,21 @@
+# vim: set noet:
+
+require 'test/unit'
+require 'wang'
+
+class WangTest < Test::Unit::TestCase
+  def setup
+    @client = WANG.new
+  end
+
+  def test_returns_success_from_google
+    status, headers, body = @client.get('http://www.google.com')
+    assert_equal 200, status
+  end
+
+	def test_returns_headers_hash
+		status, headers, body = @client.get('http://www.google.com')
+		assert headers.is_a?(Hash)
+		assert_equal 'text/html; charset=UTF-8', headers['content-type']
+	end
+end
